52be0466e7909b6cedfa41a69d515582d3279cb4
Divakar Raut <divakar.raut@service.nsw.gov.au>
2021-06-25 20:56:24 +1000

    read and write from google sheet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,118 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import NameList from './NameList'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFutbol } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faFutbol)
 function App() {
+
+const initialName = {
+  name: '',
+  key : ''
+}
+
+const [venue, setvenue] = useState('')
+const [formData, setFormData] = useState({
+  name:" ",
+  key: " "
+})
+const [data, setData] = useState('')
+const [nameList, setNameList] = useState([])
+const [currentName, setCurrentName] = useState(initialName)
+
+
+useEffect(() => {
+  
+  fetch(`https://spreadsheets.google.com/feeds/list/1y_za4ROstVZN6g4XS38nOIDZgVIVXngVcZcbNziJjjs/od6/public/basic?alt=json`)
+      .then(res => res.json())
+      .then(json => setData(json.feed.entry))
+         
+}, [])
+
+
+const players =[];
+for (let index = 0; index < data.length; index++) {
+ 
+  const element = data[index];
+  
+   const user = {
+     name:element.title,
+     key: Date.now()
+   }
+  //  setNameList(...nameList,user)
+ players.push(user)
+ console.log("This is Player>>>", players)
+ 
+  
+}
+
+
+const {name, key } = currentName;
+
+
+const addItem = async (e) => {
+  e.preventDefault();
+  const newName = currentName;
+  if(currentName.name !==""){
+
+    const names = [...nameList, newName];
+  setNameList( names)
+    setCurrentName(initialName)
+  
+}
+
+try {
+
+  const response = await fetch('https://v1.nocodeapi.com/thewalker/google_sheets/WIddlvQOkPypNWpI?tabId=Sheet1',
+  {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify([[name, key]])
+
+  })
+  await response.json();
+
+
+  
+} catch (error) {
+  console.log(error)
+}
+
+
+
+}
+
+const handleInput = (e) => {
+  const playerName = {
+    name: e.target.value,
+    key: Date.now()
+  }
+ setCurrentName(playerName)
+}
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div id='title'> Hamro Club Oran Park </div>
+        <div id='venue'><b>Venue:</b> This is static Venue</div>
+        <div id='date'><b>Date:</b> 26/06/2021, Saturday </div>
+        <form id="to-do-form" onSubmit={addItem}>
+          <input type="text" placeholder="Enter Name" value= {currentName.name} onChange={handleInput}></input>
+          <button type="submit">Add</button>
+        </form>
+        
+                    
+          <NameList data={data} players={players} nameList={nameList} />
+
+                 
       </header>
     </div>
   );
---
+++ src/NameList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './NameList.css';
+import FlipMove from 'react-flip-move';
+
+function NameList(props){
+    
+  const nameList = props.nameList
+  const players = props.players
+
+  const data = props.data
+    
+    
+
+    
+   
+
+  //   const listNames = nameList.map(name =>
+  //  {
+  //      return <div className="list" key={name.key}>
+  //    <p>
+  //        <input type="text" id={name.key} value={name.name} />
+      
+  //    </p>
+     
+  //   </div>})
+
+
+const listNames1 = players.map((player,i) =>
+  {
+      return <div className="list" key={player.name}>
+    <p>
+       {i +1} <input type="text" id={player.key} value={player.name.$t} />
+     
+    </p>
+    
+   </div>})
+
+    return <div>
+        <FlipMove duration={300} easing="ease-in-out">
+        {listNames1}
+        </FlipMove>
+    
+    </div>;
+  }
+
+  export default NameList;
